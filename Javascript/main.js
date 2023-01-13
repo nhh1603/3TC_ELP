49f14f5f76c36031b3d310fc6b859731cd233cc4
nhh1603 <nghiahieu12@gmail.com>
2023-01-13 15:46:02 +0100

    tab

--- Javascript/main.js
+++ Javascript/main.js
@@ -2,20 +2,19 @@ const readline = require('readline');
 const { exec } = require('child_process');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-//while (true) {
-    rl.question('Enter a command: ', (command) => {
-      exec(command, (error, stdout, stderr) => {
+rl.question('Enter a command: ', (command) => {
+    exec(command, (error, stdout, stderr) => {
         if (error) {
-          console.error(`exec error: ${error}`);
-          return;
+            console.error(`exec error: ${error}`);
+            return;
         }
         console.log(`stdout: ${stdout}`);
         console.log(`stderr: ${stderr}`);
-      });
-      rl.close();
     });
-//}
+    rl.close();
+});
+
