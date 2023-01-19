a0668651f767dffab2a51775e24ce63d33b1e462
Iman <reyan.toumi@gmail.com>
2023-01-19 22:25:30 +0100

    Added getConvolvedWithin function which computes the convolution of a part (horizontal for now) of an image with a kernel. This is the generalization of gaussianBlur function with the raw result pixels returned

--- Go/blur_jpeg.go
+++ Go/blur_jpeg.go
@@ -7,6 +7,56 @@ import (
 	"math"
 )
 
+// Computes the convolution of the pixels in the range [startRow, endRow] with the given kernel
+func getConvolvedWithin(img image.Image, startRow, endRow int, kernel [][]float64) [][]color.RGBA {
+	bounds := img.Bounds()
+
+	if endRow >= bounds.Max.Y {
+		panic("End row is out of image's bounds")
+	} else if startRow < bounds.Min.Y {
+		panic("Start row is out of image's bounds")
+	}
+
+	resultPixels := make([][]color.RGBA, endRow-startRow+1)
+	radius := (len(kernel) - 1) / 2
+
+	// Iterate over each pixel from the starting to the ending row of pixels
+	for y := startRow; y <= endRow; y++ {
+		if resultPixels[y] == nil {
+			resultPixels[y] = make([]color.RGBA, bounds.Max.X-bounds.Min.X)
+		}
+		pixelRow := resultPixels[y]
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			var r, g, b float64
+
+			// Iterate over each surrounding pixel within the specified radius
+			for i := -radius; i <= radius; i++ {
+				for j := -radius; j <= radius; j++ {
+					neighborX := x + i
+					neighborY := y + j
+					if neighborX < bounds.Min.X || neighborX >= bounds.Max.X ||
+						neighborY < bounds.Min.Y || neighborY >= bounds.Max.Y {
+						// Ignore neighbor pixels out of range
+						continue
+					}
+					// Get the color of a surrounding pixel
+					sr, sg, sb, _ := img.At(neighborX, neighborY).RGBA()
+
+					// Apply the Gaussian blur weight to the surrounding pixel color
+					weight := kernel[i+radius][j+radius]
+					r += float64(sr>>8) * weight
+					g += float64(sg>>8) * weight
+					b += float64(sb>>8) * weight
+				}
+			}
+			// Set the color of the destination pixel
+			pixelRow[x] = color.RGBA{uint8(r), uint8(g), uint8(b), 255}
+		}
+	}
+
+	return resultPixels
+}
+
 func gaussianBlur(img image.Image, radius int) *image.RGBA {
 	// Create a new RGBA image with the same bounds as the original
 	bounds := img.Bounds()
@@ -25,6 +75,7 @@ func gaussianBlur(img image.Image, radius int) *image.RGBA {
 					xn := x + i
 					yn := y + j
 					if xn < bounds.Min.X || xn >= bounds.Max.X || yn < bounds.Min.Y || yn >= bounds.Max.Y {
+						// Ignore neighbor pixels out of range
 						continue
 					}
 					neighborPixelColor := img.At(xn, yn)
@@ -52,6 +103,7 @@ func generateKernel(radius int) [][]float64 {
 	kernel := make([][]float64, size)
 	sqrSigma := math.Max(float64(radius*radius)/4, 1)
 	sum := 0.0
+
 	for i := -radius; i <= radius; i++ {
 		kernel[i+radius] = make([]float64, size)
 		for j := -radius; j <= radius; j++ {
@@ -72,7 +124,7 @@ func generateKernel(radius int) [][]float64 {
 
 // 2D Gaussian function
 func gaussianWeight(x int, y int, sqrSigma float64) float64 {
-	return 1 / (2 * math.Pi * float64(sqrSigma)) * math.Exp(-(float64(x*x+y*y))/(2*float64(sqrSigma)))
+	return 1 / (2 * math.Pi * sqrSigma) * math.Exp(-(float64(x*x+y*y))/(2*sqrSigma))
 }
 
 func main() {
