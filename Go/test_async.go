e85322fc841daf8ce0f27c6b805fa2ba02e62f97
Iman <reyan.toumi@gmail.com>
2023-01-20 17:45:51 +0100

    Added CTRL-P interrupt to exit CLI

--- Go/test_async.go
+++ Go/test_async.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"sync"
 	"time"
 )
 
 var wg sync.WaitGroup // instanciation de notre structure WaitGroup
+
+func run(name string) {
+	defer wg.Done()
+	for i := 0; i < 3; i++ {
+		time.Sleep(3 * time.Second)
+		fmt.Println(name, " : ", i)
+	}
+}
 
 func computeSum(array *[]int) int {
 	sum := 0
@@ -25,15 +34,26 @@ func computeSumAsync(array *[]int, start, end int, resChan chan int) {
 	resChan <- sum
 }
 
-func main2() {
+func computeSumAsync2(array *[]int, resChan chan int, taskChan chan *[2]int) {
+	for bounds := range taskChan {
+		sum := 0
+		for i := bounds[0]; i < bounds[1]; i++ {
+			sum += (*array)[i]
+		}
+		resChan <- sum
+	}
+}
 
-	const ARRAY_SIZE = 100000000
+const ARRAY_SIZE = 10000000
+
+func runResultChannel() {
 	arr := make([]int, ARRAY_SIZE)
 
 	for i := 0; i < ARRAY_SIZE; i++ {
 		arr[i] = rand.Intn(10)
 	}
 
+	println("RESULT CHANNEL VERSION")
 	println("Sync version ****")
 
 	startTime := time.Now()
@@ -42,7 +62,7 @@ func main2() {
 	println("Time: ", endTime.Sub(startTime).Milliseconds())
 	println()
 
-	const WORKERS_COUNT = 8
+	const WORKERS_COUNT = 4
 	resChan := make(chan int, WORKERS_COUNT)
 
 	wg.Add(WORKERS_COUNT)
@@ -53,16 +73,69 @@ func main2() {
 		go computeSumAsync(&arr, startIndex, end, resChan)
 	}
 	wg.Wait()
-	endTime = time.Now()
 	close(resChan)
 
 	result := 0
 	for partialRes := range resChan {
 		result += partialRes
 	}
+	endTime = time.Now()
 
 	println("Async version ****")
 	println("Result: ", result)
 	println("Time: ", endTime.Sub(startTime).Milliseconds())
+}
 
+func runTaskChannel() {
+	arr := make([]int, ARRAY_SIZE)
+
+	for i := 0; i < ARRAY_SIZE; i++ {
+		arr[i] = rand.Intn(10)
+	}
+
+	println("TASK CHANNEL VERSION")
+	println("Sync version ****")
+
+	startTime := time.Now()
+	println("Result: ", computeSum(&arr))
+	endTime := time.Now()
+	println("Time: ", endTime.Sub(startTime).Milliseconds())
+	println()
+
+	const WORKERS_COUNT = 4
+	const TASK_COUNT = 50
+	resChan := make(chan int, WORKERS_COUNT)
+	taskChan := make(chan *[2]int, TASK_COUNT)
+
+	startTime = time.Now()
+	for i := 0; i < WORKERS_COUNT; i++ {
+		go computeSumAsync2(&arr, resChan, taskChan)
+	}
+
+	// Put the tasks in the channel and then close the channel
+	for i := 0; i < TASK_COUNT; i++ {
+		startIndex := (ARRAY_SIZE / TASK_COUNT) * i
+		end := (ARRAY_SIZE / TASK_COUNT) * (i + 1)
+		taskChan <- &[2]int{startIndex, end}
+	}
+	close(taskChan)
+
+	retrievedResCount := 0
+	result := 0
+	for partialRes := range resChan {
+		result += partialRes
+		retrievedResCount++
+		if retrievedResCount == TASK_COUNT {
+			break
+		}
+	}
+	endTime = time.Now()
+
+	println("Async version ****")
+	println("Result: ", result)
+	println("Time: ", endTime.Sub(startTime).Milliseconds())
+}
+
+func main() {
+	runTaskChannel()
 }
--- Javascript/console_loop.js
+++ Javascript/console_loop.js
@@ -1,10 +1,21 @@
 readLine = require('readline')
+const { exit } = require('process');
 const { promisify } = require('util')
 const execPromise = promisify(require('child_process').exec);
 
 const rl = readLine.createInterface({
     input: process.stdin,
     output: process.stdout
+})
+
+// Bind keypress event for CTRL-P interrupt
+readLine.emitKeypressEvents(process.stdin)
+process.stdin.setRawMode(true)
+process.stdin.on('keypress', (_str, key) => {
+    if (key.ctrl && key.name == 'p') {
+        console.log()
+        exit(0)
+    }
 })
 
 const askCommand = () => {
