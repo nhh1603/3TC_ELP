e8af157e781b79261630ab7462425d509558d878
Iman974 <reyan.toumi@gmail.com>
2023-01-26 21:53:03 +0100

    Code cleanup & optimizations: - removed resChan and used WaitGroup instead - replaced currentPix [2]int with image.Point{X,Y} - inverted coordinates of currentPix for consistency - changed taskChan type to *[2]int (arrays are faster than slices) - moved kernel generation to generateImage function - removed unused functions in blurTools - removed test_async file - restored panic in convolveRange to stay strict on passed params - [Fixed] When there is a remainder, last pixels are not drawn because goroutines end before completing entire img - changed taskChan size from TASKS_COUNT to WORKERS_COUNT

--- Go/blurTools.go
+++ Go/blurTools.go
@@ -1,39 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"math"
-	"time"
 )
 
 // Computes the convolution of the pixels in the range [startPix, endPix] with the given kernel
 func convolveRange(fromImg image.Image, toImg *image.RGBA, startPix, endPix int, kernel [][]float64) {
 	fromBounds := fromImg.Bounds()
+	maxBounds := fromBounds.Max
+	minBounds := fromBounds.Min
 
-	if endPix >= fromBounds.Max.X * fromBounds.Max.Y {
-		// panic("End row is out of image's bounds")
-		endPix = fromBounds.Max.X * fromBounds.Max.Y - 1
+	if endPix >= maxBounds.X*maxBounds.Y {
+		panic("End pixel is out of image's bounds")
 	} else if startPix < 0 {
-		// panic("Start row is out of image's bounds")
-		startPix = 0
+		panic("Start pixel is out of image's bounds")
 	}
 
 	radius := (len(kernel) - 1) / 2
 
-	// Iterate over each pixel from the starting to the ending row of pixels
-	for i := startPix; i <= endPix; i++ {
-		currentPix := [2]int {i / fromBounds.Max.X, i % fromBounds.Max.X}
+	// Iterate over each pixel from startPix to endPix, row after row
+	for pixIndex := startPix; pixIndex <= endPix; pixIndex++ {
+		currentPix := image.Point{X: pixIndex % maxBounds.X, Y: pixIndex / maxBounds.X}
 		var r, g, b float64
 		// Iterate over each surrounding pixel within the specified radius
 		for i := -radius; i <= radius; i++ {
 			for j := -radius; j <= radius; j++ {
-				neighborX := currentPix[1] + i
-				neighborY := currentPix[0] + j
-				if neighborX < fromBounds.Min.X || neighborX >= fromBounds.Max.X ||
-					neighborY < fromBounds.Min.Y || neighborY >= fromBounds.Max.Y {
-					// Ignore neighbor pixels out of range
+				neighborX := currentPix.X + i
+				neighborY := currentPix.Y + j
+				if neighborX < minBounds.X || neighborX >= maxBounds.X ||
+					neighborY < minBounds.Y || neighborY >= maxBounds.Y {
+					// Ignore neighbor pixels out of image bounds
 					continue
 				}
 				// Get the color of a surrounding pixel
@@ -46,146 +44,12 @@ func convolveRange(fromImg image.Image, toImg *image.RGBA, startPix, endPix int,
 			}
 		}
 		// Set the color of the destination pixel
-		toImg.Set(currentPix[1], currentPix[0], color.RGBA{uint8(r), uint8(g), uint8(b), 255})
+		toImg.Set(currentPix.X, currentPix.Y, color.RGBA{uint8(r), uint8(g), uint8(b), 255})
 	}
-}
-
-// Computes the convolution of the pixels in the range [startRow, endRow] with the given kernel
-func convolveRegion(fromImg image.Image, toImg *image.RGBA, region image.Rectangle, kernel [][]float64) {
-	fromBounds := fromImg.Bounds()
-
-	if region.Max.Y > fromBounds.Max.Y {
-		panic("End row is out of image's bounds")
-	} else if region.Min.Y < fromBounds.Min.Y {
-		panic("Start row is out of image's bounds")
-	}
-
-	radius := (len(kernel) - 1) / 2
-
-	// Iterate over each pixel from the starting to the ending row of pixels
-	for x := region.Min.X; x < region.Max.X; x++ {
-		for y := region.Min.Y; y < region.Max.Y; y++ {
-			var r, g, b float64
-
-			// Iterate over each surrounding pixel within the specified radius
-			for i := -radius; i <= radius; i++ {
-				for j := -radius; j <= radius; j++ {
-					neighborX := x + i
-					neighborY := y + j
-					if neighborX < fromBounds.Min.X || neighborX >= fromBounds.Max.X ||
-						neighborY < fromBounds.Min.Y || neighborY >= fromBounds.Max.Y {
-						// Ignore neighbor pixels out of range
-						continue
-					}
-					// Get the color of a surrounding pixel
-					neighborR, neighborG, neighborB, _ := fromImg.At(neighborX, neighborY).RGBA()
-
-					// Apply the Gaussian blur weight to the surrounding pixel color
-					weight := kernel[i+radius][j+radius]
-					r += float64(neighborR>>8) * weight
-					g += float64(neighborG>>8) * weight
-					b += float64(neighborB>>8) * weight
-				}
-			}
-			// Set the color of the destination pixel
-			toImg.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), 255})
-		}
-	}
-}
-
-// Computes the convolution of the pixels in the range [startRow, endRow] with the given kernel
-func getConvolvedWithin(img image.Image, startRow, endRow int, kernel [][]float64) [][]color.RGBA {
-	bounds := img.Bounds()
-
-	if endRow >= bounds.Max.Y {
-		panic("End row is out of image's bounds")
-	} else if startRow < bounds.Min.Y {
-		panic("Start row is out of image's bounds")
-	}
-
-	resultPixels := make([][]color.RGBA, endRow-startRow+1)
-	radius := (len(kernel) - 1) / 2
-
-	// Iterate over each pixel from the starting to the ending row of pixels
-	for y := startRow; y <= endRow; y++ {
-		if resultPixels[y] == nil {
-			resultPixels[y] = make([]color.RGBA, bounds.Max.X-bounds.Min.X)
-		}
-		pixelRow := resultPixels[y]
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			var r, g, b float64
-
-			// Iterate over each surrounding pixel within the specified radius
-			for i := -radius; i <= radius; i++ {
-				for j := -radius; j <= radius; j++ {
-					neighborX := x + i
-					neighborY := y + j
-					if neighborX < bounds.Min.X || neighborX >= bounds.Max.X ||
-						neighborY < bounds.Min.Y || neighborY >= bounds.Max.Y {
-						// Ignore neighbor pixels out of range
-						continue
-					}
-					// Get the color of a surrounding pixel
-					neighborR, neighborG, neighborB, _ := img.At(neighborX, neighborY).RGBA()
-
-					// Apply the Gaussian blur weight to the surrounding pixel color
-					weight := kernel[i+radius][j+radius]
-					r += float64(neighborR>>8) * weight
-					g += float64(neighborG>>8) * weight
-					b += float64(neighborB>>8) * weight
-				}
-			}
-			// Set the color of the destination pixel
-			pixelRow[x] = color.RGBA{uint8(r), uint8(g), uint8(b), 255}
-		}
-	}
-
-	return resultPixels
-}
-
-func gaussianBlur(img image.Image, radius int) *image.RGBA {
-	// Create a new RGBA image with the same bounds as the original
-	bounds := img.Bounds()
-	destImg := image.NewRGBA(bounds)
-	kernel := generateKernel(radius)
-
-	// Iterate over each pixel in the image
-	for x := bounds.Min.X; x < bounds.Max.X; x++ {
-		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-			var r, g, b float64
-
-			// Iterate over each surrounding pixel within the specified radius
-			for i := -radius; i <= radius; i++ {
-				for j := -radius; j <= radius; j++ {
-					// Get the color of a surrounding pixel
-					neighborX := x + i
-					neighborY := y + j
-					if neighborX < bounds.Min.X || neighborX >= bounds.Max.X ||
-						neighborY < bounds.Min.Y || neighborY >= bounds.Max.Y {
-						// Ignore neighbor pixels out of range
-						continue
-					}
-					neighborPixelColor := img.At(neighborX, neighborY)
-					neighborR, neighborG, neighborB, _ := neighborPixelColor.RGBA()
-
-					// Apply the Gaussian blur weight to the surrounding pixel color
-					blurWeight := kernel[i+radius][j+radius]
-					r += float64(neighborR>>8) * blurWeight
-					g += float64(neighborG>>8) * blurWeight
-					b += float64(neighborB>>8) * blurWeight
-				}
-			}
-
-			// Set the color of the destination pixel
-			destImg.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), 255})
-		}
-	}
-
-	return destImg
 }
 
 // Generate a normalized gaussian kernel with given radius
-func generateKernel(radius int) [][]float64 {
+func generateGaussKernel(radius int) [][]float64 {
 	size := radius*2 + 1
 	kernel := make([][]float64, size)
 	sqrSigma := math.Max(float64(radius*radius)/4, 1)
@@ -212,20 +76,4 @@ func generateKernel(radius int) [][]float64 {
 // 2D Gaussian function
 func gaussianWeight(x int, y int, sqrSigma float64) float64 {
 	return 1 / (2 * math.Pi * sqrSigma) * math.Exp(-(float64(x*x+y*y))/(2*sqrSigma))
-}
-
-// Function main to test  
-func main2() {
-	fmt.Println("Start")
-
-	var img image.Image = loadImage("assets/original.jpg")
-	destImg := image.NewRGBA(img.Bounds())
-	// Apply the Gaussian blur
-	// convolveRegion(img, destImg, image.Rect(50, 100, 200, 190), generateKernel(5))
-	start := time.Now()
-	convolveRange(img, destImg, 10, 4000, generateKernel(5))
-	end := time.Now()
-	saveImage("assets/original_blurred.png", destImg, JPEG)
-	fmt.Println("Blurred image generated")
-	fmt.Println("Time: ", end.Sub(start).Milliseconds())
 }
--- Go/main.go
+++ Go/main.go
@@ -3,52 +3,61 @@ package main
 import (
 	"fmt"
 	"image"
+	"sync"
 	"time"
 )
 
 const TASKS_COUNT = 50
 const WORKERS_COUNT = 8
 
-func worker(fromImg image.Image, toImg *image.RGBA, taskChan chan []int, resChan chan int) {
+var waitGroup sync.WaitGroup
+
+func worker(fromImg image.Image, toImg *image.RGBA, taskChan chan *[2]int, kernel [][]float64) {
 	for t := range taskChan {
-		convolveRange(fromImg, toImg, t[0], t[1], generateKernel(5))
-		resChan <- 1
+		convolveRange(fromImg, toImg, t[0], t[1], kernel)
 	}
+	waitGroup.Done()
 }
 
-func generateImage(fromImg image.Image, toImg *image.RGBA) {
-	totalPixels := fromImg.Bounds().Max.X*fromImg.Bounds().Max.Y
-	taskPerWorker := totalPixels / TASKS_COUNT
+func generateBlurImage(fromImg image.Image) *image.RGBA {
+	fromBounds := fromImg.Bounds()
+	totalPixels := fromBounds.Max.X * fromBounds.Max.Y
+	destImg := image.NewRGBA(fromBounds)
+	pixelsPerWorker := totalPixels / TASKS_COUNT
+	kernel := generateGaussKernel(5)
 
-	taskChan := make(chan []int, TASKS_COUNT)
-	resChan := make(chan int, TASKS_COUNT)
+	taskChan := make(chan *[2]int, WORKERS_COUNT)
 
+	waitGroup.Add(WORKERS_COUNT)
 	for i := 0; i < WORKERS_COUNT; i++ {
-		go worker(fromImg, toImg, taskChan, resChan)
+		go worker(fromImg, destImg, taskChan, kernel)
 	}
 
-	for j := 0; j < TASKS_COUNT; j++ {
-		taskChan <- []int{j*taskPerWorker, (j+1)*taskPerWorker-1}
+	for i := 0; i < TASKS_COUNT; i++ {
+		startPixel := i * pixelsPerWorker
+		endPixel := (i+1)*pixelsPerWorker - 1
+		taskChan <- &[2]int{startPixel, endPixel}
 	}
+	// The last task may have at most TASKS_COUNT additional pixels compared to other tasks,
+	// but considering this const is relatively small, it's fine to let the last goroutine finish up.
+	taskChan <- &[2]int{TASKS_COUNT * pixelsPerWorker, totalPixels - 1}
 	close(taskChan)
 
 	// Assure that all goroutines have completed
-	for a := 0; a < TASKS_COUNT; a++ {
-		<-resChan
-	}
-	
-	saveImage("assets/original_blurred.png", toImg, JPEG)
+	waitGroup.Wait()
+
+	return destImg
 }
 
 func main() {
 	fmt.Println("Start")
 
 	var img image.Image = loadImage("assets/original.jpg")
-	destImg := image.NewRGBA(img.Bounds())
 
 	start := time.Now()
-	generateImage(img, destImg)
+	blurredImg := generateBlurImage(img)
 	end := time.Now()
+	saveImage("assets/original_blurred.jpg", blurredImg, JPEG)
 	fmt.Println("Blurred image generated")
 	fmt.Println("Time: ", end.Sub(start).Milliseconds())
 }
