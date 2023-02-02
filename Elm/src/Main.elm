81e725b46b9700be548526ec0f7f7baed715a251
Iman974 <reyan.toumi@gmail.com>
2023-02-02 23:56:14 +0100

    Added several helper functions to retrieve definitions, created type alias Meaning

--- Elm/src/Main.elm
+++ Elm/src/Main.elm
@@ -1,11 +1,13 @@
 module Main exposing (..)
 
+-- import Json.Decode exposing (Decoder, map2, field, int, string, decodeString, at, index)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (..)
 import Http
-import Json.Decode exposing (Decoder, map2, field, int, string)
+import Json.Decode exposing (..)
 
 
 
@@ -14,12 +16,12 @@ import Json.Decode exposing (Decoder, map2, field, int, string)
 
 main : Program () Model Msg
 main =
-  Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
-    }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 
@@ -27,9 +29,10 @@ main =
 
 
 type Model
-  = Failure
-  | Loading
-  | Success Word
+    = Failure
+    | Loading
+    | Success Word
+
 
 
 -- type alias Quote =
@@ -40,15 +43,21 @@ type Model
 --   }
 
 
+type alias Meaning =
+    { partOfSpeech : String
+    , definitions : String
+    }
+
+
 type alias Word =
-  { word : String
-  , phonetic : String
-  }
+    { word : String
+    , meanings : Meaning
+    }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
-  (Loading, getRandomWord)
+    ( Loading, getRandomWord )
 
 
 
@@ -56,23 +65,23 @@ init _ =
 
 
 type Msg
-  = MorePlease
-  | GotWord (Result Http.Error Word)
+    = MorePlease
+    | GotWord (Result Http.Error Word)
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    MorePlease ->
-      (Loading, getRandomWord)
+    case msg of
+        MorePlease ->
+            ( Loading, getRandomWord )
 
-    GotWord result ->
-      case result of
-        Ok word ->
-          (Success word, Cmd.none)
+        GotWord result ->
+            case result of
+                Ok word ->
+                    ( Success word, Cmd.none )
 
-        Err _ ->
-          (Failure, Cmd.none)
+                Err _ ->
+                    ( Failure, Cmd.none )
 
 
 
@@ -81,7 +90,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
 
 
 
@@ -90,34 +99,34 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h2 [] [ text "Random Quotes" ]
-    , viewWord model
-    ]
+    div []
+        [ h2 [] [ text "Random Quotes" ]
+        , viewWord model
+        ]
 
 
 viewWord : Model -> Html Msg
 viewWord model =
-  case model of
-    Failure ->
-      div []
-        [ text "I could not load a random quote for some reason. "
-        , button [ onClick MorePlease ] [ text "Try Again!" ]
-        ]
+    case model of
+        Failure ->
+            div []
+                [ text "I could not load a random quote for some reason. "
+                , button [ onClick MorePlease ] [ text "Try Again!" ]
+                ]
 
-    Loading ->
-      text "Loading..."
+        Loading ->
+            text "Loading..."
 
-    Success word ->
-      div []
-        [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
-        , blockquote [] [ text word.word ]
-        , p [ style "text-align" "right" ]
-            [ text "— "
-            , cite [] [ text word.word ]
-            , text (" by " ++ word.phonetic)
-            ]
-        ]
+        Success word ->
+            div []
+                [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
+                , blockquote [] [ text word.word ]
+                , p [ style "text-align" "right" ]
+                    [ text "— "
+                    , cite [] [ text word.word ]
+                    -- , text (" by " ++ word.phonetic)
+                    ]
+                ]
 
 
 
@@ -126,14 +135,40 @@ viewWord model =
 
 getRandomWord : Cmd Msg
 getRandomWord =
-  Http.get
-    { url = "https://api.dictionaryapi.dev/api/v2/entries/en/computer"
-    , expect = Http.expectJson GotWord wordDecoder
-    }
+    Http.get
+        { url = "https://api.dictionaryapi.dev/api/v2/entries/en/computer"
+        , expect = Http.expectJson GotWord wordDecoder
+        }
 
 
 wordDecoder : Decoder Word
 wordDecoder =
-  map2 Word
-    (field "word" string)
-    (field "phonetic" string)
+    map2 Word
+        (index 0 (field "word" string))
+        meaningDecoder
+
+meaningDecoder : Decoder Meaning
+meaningDecoder =
+    map2 Meaning
+        (getMeanings (getStringInObjArray 0 "partOfSpeech"))
+        (getMeanings (getArrayInObjArray 0 "definitions" (getStringInObjArray 0 "definition")))
+
+getDefinition : Int -> Decoder String
+getDefinition definitionIndex = getStringInObjArray definitionIndex "definitions"
+
+
+getMeanings : Decoder a -> Decoder a
+getMeanings decoder =
+    index 0 (field "meanings" decoder)
+
+
+getStringInObjArray : Int -> String -> Decoder String
+getStringInObjArray objectIndex fieldName =
+    index objectIndex (getStringField fieldName)
+
+getArrayInObjArray : Int -> String -> Decoder a -> Decoder a
+getArrayInObjArray objectIndex arrayName decoder = index objectIndex (field arrayName decoder)
+
+getStringField : String -> Decoder String
+getStringField name =
+    field name string
